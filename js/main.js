402d7a3a3e913561d6ba24aa1e6381f30914b2ba
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-08-09 12:22:25 -0600

    add checkwinner

--- js/main.js
+++ js/main.js
@@ -29,20 +29,25 @@ Game.prototype.nextPlayer = function() {
 Board.prototype.makeMove = function(cellID, team) {
   this.moveArr[cellID] = team;
   console.log(this.moveArr);
-  this.checkWinner();
+  console.log(this.cellID);
 };
 Board.prototype.winCondition = [
   [0,1,2],[3,4,5],[6,7,8],
   [0,3,6],[1,4,7],[2,5,8],
   [0,4,8],[2,4,6]
  ];
- Board.prototype.checkWinner = function() {
-  for (var i = 0; i < this.winCondition; i++) {
-    if (this.moveArr.indexOf(winCondition[i]) > 0) {
-      console.log('we have a winner');
+ Board.prototype.checkWinner = function(cellID) {
+  for (var i = 0; i < this.checkWinner.length; i++) {
+    if ((this.checkWinner[i][0] == this.moveArr[cellID])
+      && (this.checkWinner[i][1] == this.moveArr[cellID])
+      && (this.checkWinner[i][2] == this.moveArr[cellID])) {
+       console.log('winner');
+    } else {
+      console.log('no winner');
     }
   }
- };
+};
+
 
 $(document).ready(function() {
   var game = new Game();
@@ -54,7 +59,9 @@ $(document).ready(function() {
       $(this).text(game.currentPlayer.team);
       var cellID = $(this).attr('id');
       game.board.makeMove(cellID, game.currentPlayer.team);
+      console.log(cellID);
       game.nextPlayer();
+      game.board.checkWinner(cellID);
 
     } else {
       alert('space already taken you fool!');
